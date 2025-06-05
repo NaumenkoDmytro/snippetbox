570e21b20b4fe6b8af6a6c9e4981d5332bfda009
Dima <naymenko.dmitry5@gmail.com>
2025-06-05 11:39:10 +0300

    user model added

--- internal/models/errors.go
+++ internal/models/errors.go
@@ -2,4 +2,10 @@ package models
 
 import "errors"
 
-var ErrNoRecord = errors.New("models: no matching found")
+var (
+	ErrNoRecord = errors.New("models: no matching found")
+
+	ErrInvalidCredentials = errors.New("models: invalid credentials")
+
+	ErrDuplicateEmail = errors.New("models: duplicate email")
+)
