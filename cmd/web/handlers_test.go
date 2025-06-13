8ffd270f2cd8fc609079a8b12033ee1ae8a71fa0
Dima <naymenko.dmitry5@gmail.com>
2025-06-13 17:58:04 +0300

    end-to-end test added "base"

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,35 +1,72 @@
 package main
 
 import (
-	"bytes"
-	"io"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"snippetbox.dmytron.gogo/internal/assert"
 )
 
+// func TestPing(t *testing.T) {
+// 	rr := httptest.NewRecorder()
+
+// 	r, err := http.NewRequest(http.MethodGet, "/", nil)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+
+// 	ping(rr, r)
+
+// 	rs := rr.Result()
+
+// 	assert.Equal(t, rs.StatusCode, http.StatusOK)
+
+// 	defer rs.Body.Close()
+// 	body, err := io.ReadAll(rs.Body)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	body = bytes.TrimSpace(body)
+
+// 	assert.Equal(t, string(body), "OK")
+// }
+
+// Decomposing of the large test example
+
+// func TestPing(t *testing.T) {
+// 	app := &application{
+// 		logger: slog.New(slog.DiscardHandler),
+// 	}
+
+// 	ts := httptest.NewTLSServer(app.routes())
+// 	defer ts.Close()
+
+// 	rs, err := ts.Client().Get(ts.URL + "/ping")
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+
+// 	assert.Equal(t, rs.StatusCode, http.StatusOK)
+
+// 	defer rs.Body.Close()
+// 	body, err := io.ReadAll(rs.Body)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+
+// 	body = bytes.TrimSpace(body)
+
+// 	assert.Equal(t, string(body), "OK")
+// }
+
 func TestPing(t *testing.T) {
-	rr := httptest.NewRecorder()
+	app := newTestApplication(t)
 
-	r, err := http.NewRequest(http.MethodGet, "/", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
 
-	ping(rr, r)
+	code, _, body := ts.get(t, "/ping")
 
-	rs := rr.Result()
-
-	assert.Equal(t, rs.StatusCode, http.StatusOK)
-
-	defer rs.Body.Close()
-	body, err := io.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	body = bytes.TrimSpace(body)
-
-	assert.Equal(t, string(body), "OK")
+	assert.Equal(t, code, http.StatusOK)
+	assert.Equal(t, body, "OK")
 }
